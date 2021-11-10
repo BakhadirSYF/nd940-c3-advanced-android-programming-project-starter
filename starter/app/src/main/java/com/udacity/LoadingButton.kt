8bb424abb5b12450bbafdd0c20c589c7d623f220
Bakhadir Rakhmatov <bakhadir.rakhmatov@syf.com>
2021-11-10 08:54:20 -0600

    Circle progress indicator

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,16 +1,14 @@
 package com.udacity
 
-import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
-import android.renderscript.Sampler
 import android.util.AttributeSet
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
 import android.view.animation.*
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.min
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -25,13 +23,18 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
-    internal var valueAnimator = ValueAnimator()
+    private var progressCircleSize = 0f
+
+    private var linearProgressAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
+    private var buttonText = context.getString(R.string.download_button_label)
+
     private val rectF: RectF = RectF(0F, 0F, widthSize.toFloat(), heightSize.toFloat())
+    private val circleF = RectF()
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
@@ -53,20 +56,37 @@ class LoadingButton @JvmOverloads constructor(
         extraBitmap = Bitmap.createBitmap(widthSize, heightSize, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+
+        progressCircleSize = (min(w, h) / 2f) * 0.4f
+    }
+
+    private fun setupCircleF() {
+        val buttonTextBounds = Rect()
+        paint.getTextBounds(buttonText, 0, buttonText.length, buttonTextBounds)
+
+        val horizontalOffset = widthSize / 2 + buttonTextBounds.width() / 2 + 48f
+        val verticalOffset = (heightSize / 2f)
+
+        circleF.set(
+            horizontalOffset - progressCircleSize,
+            verticalOffset - progressCircleSize,
+            horizontalOffset + progressCircleSize,
+            verticalOffset + progressCircleSize
+        )
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-//        Log.d("BAKHA_LOG", "onDraw")
+        Log.d("BAKHA_LOG", "onDraw")
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
 
-        rectF.recalcAfterOnMeasure(widthSize.toFloat(), heightSize.toFloat())
+        rectF.recalculateAfterOnMeasure(widthSize.toFloat(), heightSize.toFloat())
 
         // Draw button label
         paint.color = Color.BLACK
         paint.textAlign = Paint.Align.CENTER
         canvas.drawText(
-            context.getString(R.string.download_button_label),
+            buttonText,
             rectF.centerX(),
             rectF.centerY() + resources.getDimension(R.dimen.default_text_size) / 3,
             paint
@@ -85,11 +105,11 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
-//        Log.d("BAKHA_LOG", "onMeasure")
+        Log.d("BAKHA_LOG", "onMeasure")
 //        Log.d("BAKHA_LOG", "widthSize = $widthSize; heightSize = $heightSize")
     }
 
-    private fun RectF.recalcAfterOnMeasure(widthSize: Float, heightSize: Float) {
+    private fun RectF.recalculateAfterOnMeasure(widthSize: Float, heightSize: Float) {
         left = 0F
         top = 0F
         right = widthSize
@@ -98,16 +118,27 @@ class LoadingButton @JvmOverloads constructor(
 
     fun startProgressAnimation() {
         Log.d("BAKHA_LOG", "onClick")
-        val colorFrom = resources.getColor(R.color.colorPrimary, null)
+        setupCircleF()
+        setupLinearProgressAnimator()
+        linearProgressAnimator.start()
+    }
+
+    private fun setupLinearProgressAnimator() {
         val colorTo = resources.getColor(R.color.colorPrimaryDark, null)
+        val circleColor = resources.getColor(R.color.colorAccent, null)
 
         var progressColor = Paint()
         progressColor.color = colorTo
 
-        valueAnimator = ValueAnimator.ofFloat(0f, widthSize.toFloat())
-        valueAnimator.duration = 8000
-        valueAnimator.interpolator = LinearInterpolator()
-        valueAnimator.addUpdateListener {
+        val circleProgressColor = Paint(Paint.ANTI_ALIAS_FLAG)
+        circleProgressColor.style = Paint.Style.FILL
+        circleProgressColor.color = circleColor
+
+
+        linearProgressAnimator = ValueAnimator.ofFloat(0f, widthSize.toFloat())
+        linearProgressAnimator.duration = 8000
+        linearProgressAnimator.interpolator = LinearInterpolator()
+        linearProgressAnimator.addUpdateListener {
 //            Log.d("BAKHA_LOG", "valueAnimator animated value = ${it.animatedValue})")
 
             extraCanvas.drawRect(
@@ -117,13 +148,15 @@ class LoadingButton @JvmOverloads constructor(
                 heightSize.toFloat(),
                 progressColor
             )
+
+            extraCanvas.drawArc(circleF, 0f, it.animatedValue as Float, true, circleProgressColor)
+
             invalidate()
         }
-        valueAnimator.start()
     }
 
     fun updateProgressAnimation() {
-        valueAnimator.duration = 2000
-        valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+        linearProgressAnimator.duration = 2000
+        linearProgressAnimator.interpolator = AccelerateDecelerateInterpolator()
     }
 }
